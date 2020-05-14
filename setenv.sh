34636860ae3fcbaca48664245af4483eb5d18044
zhangguohong <zhangguohong@zjdyiot.com>
2020-05-14 17:36:07 +0800

    替换jsonwq->cjson

--- setenv.sh
+++ setenv.sh
@@ -1,11 +1,21 @@
-LUA_VER=`dpkg -s lua5.1 | grep 'installed'`
-if [ -z "$LUA_VER" ];
+LUA_VER5_1=`dpkg -s lua5.1 | grep 'installed'`
+if [ -z "$LUA_VER5_1" ];
 then
         echo "You need install lua5.1,run sudo apt install lua5.1"
         exit 0
 fi
 
-DEPENDS="chronos elscheduler lpack lsocket lua-fs-module lua-json luabitop luars232"
+LUA_VER5_2=`dpkg -s lua5.2 | grep 'installed'`
+LUA_VER5_3=`dpkg -s lua5.3 | grep 'installed'`
+if [ -n "$LUA_VER5_2" -o  -n "$LUA_VER5_3" ];
+then
+        echo "You need remove lua5.2 or lua5.3,run sudo apt remove lua5.2 or lua5.3"
+        exit 0
+fi
+
+
+
+DEPENDS="chronos elscheduler lpack lsocket lua-fs-module lua-cjson luabitop luars232 "
 for k in ${DEPENDS}
 do
 	echo "sudo luarocks install ${k}"
