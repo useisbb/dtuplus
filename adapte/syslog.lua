0f37246c02cd9acd542be0c2f6e14577b1d9435a
zhangguohong <zhangguohong@zjdyiot.com>
2020-05-15 14:38:27 +0800

    add prefix [?] to adapte syslog

--- adapte/syslog.lua
+++ adapte/syslog.lua
@@ -5,7 +5,7 @@ function syslog.openlog(name, _ , set_level)
 end
 
 function syslog.syslog(level, ...)
-    print(prefix .. level, ...)
+    print("[?]-" .. "[" .. prefix .. "] " .. level, ...)
 end
 -- syslog.syslog("LOG_WARNING", "Hi all " .. os.time())
 return syslog
